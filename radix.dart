576974422df04e50ebc4d2cf89b785d189cf6c71
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-10-31 13:41:42 -0300

    consertando radix

--- radix.dart
+++ radix.dart
@@ -4,14 +4,16 @@
 import 'dart:math';
 
 void main() {
-  List<int> lst = [10, 8, 17, 2, 13, 1];
+  List<int> lst = [10, 8, 17, 2, 13, 1, 22];
   int foo(dynamic x) => x;
-
-  // print( lst );
-  // print( normLst( lst , foo ) );
-
-  binRadixSort(lst, foo);
+  List<int> rdx = normLst(lst, foo);
   print(lst);
+  print(rdx);
+
+  print("");
+  radixZip(lst, rdx);
+  print(lst);
+  print(rdx);
 }
 
 List<int> normLst(List baseLst, int Function(dynamic) foo) {
@@ -29,23 +31,26 @@ void radixZip(List baseLst, List<int> radixValues) {
   // para iterar
   int x;
   dynamic val;
+  Map<String, dynamic> m;
 
   int n = baseLst.length; // lembrando que ambas as listas tem mesmo tamanho
   for (int i = 0; i < n; i++) {
     val = baseLst.elementAt(i);
     x = radixValues.elementAt(i);
-    //print( "${ val } ${ x }" );
+    m = Map.fromIterables(['val', 'x'], [val, x]);
 
     lst = x.isEven ? lst1 : lst2;
-    lst.add(val);
-    // print( lst );
+    lst.add(m);
   }
 
-  // atualizando a lista
-  // baseLst.clear();
-  baseLst = lst1 + lst2;
+  baseLst.clear();
+  radixValues.clear();
+  lst = lst1 + lst2;
+  lst.forEach((entry) {
+    baseLst.add(entry['val']);
+    radixValues.add(entry['x']);
+  });
 
-  //print( "" );
   //print( baseLst );
 }
 
