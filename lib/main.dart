1ec151e340873ef0745d42aedabec57e63d424d0
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-11-08 17:00:52 -0300

    aprendendo widgets

--- lib/main.dart
+++ lib/main.dart
@@ -22,27 +22,65 @@ void f2() => runApp(
   
   //-----------------------------------------------------
   // Aprendendo Scaffolds.
+  //
+  // Um Scaffold simplesmente serve de base para juntar varias 
+  // widgets diferentes.
    MaterialApp(
 
-     home:Scaffold(
+    home:Scaffold(
 
-       //Barra no topo do App
-       appBar: AppBar(
-         title: Text( 'Hello, World' ),
-         centerTitle: true,
-         backgroundColor: Colors.blue,
-         foregroundColor: Colors.red,
-       ),
+      //Barra no topo do App
+      appBar: AppBar(
+        title: Text( 'Hello, World' ),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[300],
+        foregroundColor: Colors.red,
+      ),
 
-       // ---------------------------------------------------
-       // centro do App. O widget 'Center' centraliza todos os
-       // widgets aninhados dentro de si
-       body: Center( 
-         child: Text( 'meu primeiro app'),
-       )
-     ),
+      // ---------------------------------------------------
+      // centro do App. O widget 'Center' centraliza todos os
+      // widgets aninhados dentro de si
+      body : Center( 
+        child : Text( 
+          'meu primeiro app',
+          style : TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            fontFamily : 'indieFlower' 
+          )
+        ),
+      ),
+
+      // Botao que fica no cantinho.
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child : Text( 
+          'Click',
+          style : TextStyle(
+            fontSize: 10, 
+          ) 
+        ),
+        hoverColor: Colors.red,
+        backgroundColor: Colors.blueGrey[300],
+      ),
+    ),
 
   )
 );
 
+// --------------------------------------------------
+// A diferença entre widgets 'stateless' e 'stateful' é o 
+// fato que o primeiro tem dados estárico. Ou seja, cores,
+// texto e divs não podem mudar sem que o app seja recarrega.
+// O segundo, então, é dinâmico, com dados mutáveis
+//
+class meuApp extends StatelessWidget {
+  const ({ Key? key }) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
