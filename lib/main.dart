4af9a22c7b24cce1edc5edbb5baccc39ba2005cc
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-11-10 16:25:48 -0300

    chega por hoje

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,8 @@ void f2() => runApp(
 
    MaterialApp(
 
-    home: meuApp()
+    //home: meuApp()
+    home: outroApp()
 
   )
 );
@@ -62,26 +63,94 @@ class meuApp extends StatelessWidget {
       // centro do App. O widget 'Center' centraliza todos os
       // widgets aninhados dentro de si
       body : Center( 
-        child : Text( 
-          'Seu primeiro App',
-          style : TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            fontFamily : 'indieFlower' 
-          )
+        child : f3(),
+        // child : f4(),
+      ),
+
+      // Botao que fica no cantinho.
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => { print("nevando carai")},
+        // child : Text( 
+        //   'Click',
+        //   style : TextStyle(
+        //     fontSize: 10, 
+        //   ) 
+        // ),
+
+        child: f5(),
+        hoverColor: Colors.red,
+        backgroundColor: Colors.blueGrey[300],
+      ),
+    );
+    
+  }
+}
+
+Text f3() => Text( 
+  'Seu primeiro App',
+  style : TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    fontFamily : 'indieFlower' 
+  )
+);
+
+
+Image f4() => Image(
+  
+  // Imagens da internet
+  // image: NetworkImage( 'https://images.unsplash.com/photo-1636554613531-89ffb1592ec3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80' ),
+
+  //Imagens do proprio computador
+  image : AssetImage('images/forest.jpg'),
+  
+);
+
+// Icones não são exatamente imagens. 
+Icon f5() => Icon(
+  Icons.ac_unit_sharp,
+  color: Colors.white,
+);
+
+
+class outroApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      
+      //Barra no topo do App
+      appBar: AppBar(
+        title: Text( 'Hello, World' ),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[300],
+        foregroundColor: Colors.red,
+      ),
+
+      // ---------------------------------------------------
+      // Aprendendo a mexer com Containers
+      body : Container(
+        padding: EdgeInsets.symmetric(
+          vertical:10,
+          horizontal: 20
+        ),
+        color: Colors.grey,
+        child: Text( 'hello' ),
+        margin: EdgeInsets.symmetric(
+          vertical:30,
+          horizontal: 30
         ),
       ),
 
       // Botao que fica no cantinho.
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
+        onPressed: () => { print("nevando carai")},
         child : Text( 
           'Click',
           style : TextStyle(
             fontSize: 10, 
           ) 
         ),
-        hoverColor: Colors.red,
         backgroundColor: Colors.blueGrey[300],
       ),
     );
